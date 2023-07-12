1df1befa18d33f54ae46c7a931b5f148c8610a28
sorhawell <sorhawell@gmail.com>
2023-07-12 14:28:10 +0200

    it works!

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -7,10 +7,25 @@ fn hello_world() -> &'static str {
     "Hello world!"
 }
 
+#[extendr(use_try_from = true)]
+fn collect_dots(x: Robj, #[ellipsis] dots: Ellipsis, y: Robj) -> Result<List> {
+    let dots = dots.values()?;
+    let has_names = dots.iter().any(|v| v.name.is_some());
+
+    let dots = if has_names {
+        List::from_pairs(dots.into_iter())
+    } else {
+        List::from_values(dots.into_iter().map(|v| v.value))
+    };
+
+    Ok(list!(x = x, dots = dots, y = y))
+}
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod helloextendr;
+    fn collect_dots;
     fn hello_world;
 }
